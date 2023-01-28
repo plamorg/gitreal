5d3b0bfe18189dfa3ee53dc4e242c489db5a51bf
Lucas Sta Maria <lucas@priime.dev>
2023-01-28 15:50:33 -0500

    feat: add function to upload image to server
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,4 +2,19 @@ function App() {
   return <div></div>;
 }
 
+function uploadImage(image) {
+  const url = "localhost:8000/image/uploaddesktop/";
+  fetch(url, {
+    method: 'POST',
+    body: file,
+    headers: {
+      'content-type': image.type,
+      'content-length': `${image.size}`
+    }
+  })
+  .then(res => res.json())
+  .then(data => console.log(`Successfully uploaded image: ${data}`))
+  .catch(err => console.error(err));
+}
+
 export default App;
