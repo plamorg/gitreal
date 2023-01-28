806ece74ad0d46c7d4bbec5fb23dd31ec6bd76e2
Edward Wibowo <wibow9770@gmail.com>
2023-01-28 15:59:29 -0500

    sdfghjkl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,4 @@ function App() {
   return <div></div>;
 }
 
-function uploadImage(image) {
-  const url = "localhost:8000/image/uploaddesktop/";
-  fetch(url, {
-    method: 'POST',
-    body: file,
-    headers: {
-      'content-type': image.type,
-      'content-length': `${image.size}`
-    }
-  })
-  .then(res => res.json())
-  .then(data => console.log(`Successfully uploaded image: ${data}`))
-  .catch(err => console.error(err));
-}
-
 export default App;
--- frontend/src/UploadPage.js
+++ frontend/src/UploadPage.js
@@ -1,11 +1,28 @@
 import { useState } from "react";
 
+function uploadImage(image) {
+  const url = "localhost:8000/image/uploaddesktop/";
+  fetch(url, {
+    method: "POST",
+    body: image,
+    headers: {
+      "content-type": image.type,
+      "content-length": `${image.size}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(`Successfully uploaded image: ${data}`))
+    .catch((err) => console.error(err));
+}
+
 const UploadPage = () => {
+  const [fileURL, setFileURL] = useState();
   const [file, setFile] = useState();
 
   const handleFileChange = (e) => {
     if (e.target.files) {
-      setFile(URL.createObjectURL(e.target.files[0]));
+      setFile(e.target.files[0]);
+      setFileURL(URL.createObjectURL(e.target.files[0]));
     }
   };
 
@@ -13,8 +30,8 @@ const UploadPage = () => {
     <div>
       <main class="flex flex-col h-screen mx-auto max-w-screen-md px-4">
         <input type="file" onChange={handleFileChange} />
-        <img src={file} />
-        <a>Upload</a>
+        <img src={fileURL} />
+        <a onClick={() => uploadImage(file)}>Upload</a>
       </main>
     </div>
   );
