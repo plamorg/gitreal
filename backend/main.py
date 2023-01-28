8b1c26c722e2e8b004952fd104e50a9f6d965ab2
Lucas Sta Maria <lucas@priime.dev>
2023-01-28 14:59:58 -0500

    feat: add file/image API paths
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,60 @@
 from typing import *
+from pathlib import Path
+import datetime
+import sys
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Response
 
 app = FastAPI()
+database = dict()
+
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"info": "Hello World"}
+
+
+@app.post("/image/uploaddesktop/")
+async def create_file(file: UploadFile, username: str):
+    try:
+        contents = file.file.read()
+        time = datetime.date.today()
+        directory = "database"
+        filename = f"{directory}/{time.isoformat()}_{username}"
+        with open(filename) as f:
+            f.write(contents)
+    except:
+        return {"error": "error uploading the desktop screenshot :("}
+    finally:
+        file.file.close()
+        return {"msg": "successfully uploaded desktop file"}
+
+
+@app.get("/image/getall")
+async def get_all_files():
+    """
+    Returns a list of usernames to get the images from
+    """
+    try:
+        source_dir = Path("database/")
+        files = source_dir.iterdir()
+        filenames = [f.name for f in files]
+        return {"files": filenames}
+    except: 
+        return {"error": "failure collecting filenames"}
+
+
+# Returns an image from a given
+@app.get("/image/get", responses={200: {"content": {"image/png": {}}}}, response_class=Response)
+async def get_file(suffix_filename: str):
+    """
+    Returns an image from the database
+    """
+    try:
+        directory = "database"
+        filename = f"{directory}/{suffix_filename}"
+        with open(filename) as f:
+            content = f.read()
+            return Response(content=content, media_type="image/png")
+    except:
+        return {"error": "could not return images :("}
