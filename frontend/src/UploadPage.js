29eb031f6538901f942df75532875d562e2cc004
Edward Wibowo <wibow9770@gmail.com>
2023-01-29 08:01:55 -0500

    feat: upload webcam

--- frontend/src/UploadPage.js
+++ frontend/src/UploadPage.js
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { Center, Box, Input, Button, Image } from "@chakra-ui/react";
 import data from "./global";
+import Webcam from "react-webcam";
 
-function uploadImage(image) {
-  const url = "http://localhost:8000/image/uploaddesktop/";
+function dataURLtoFile(dataurl, filename) {
+  var arr = dataurl.split(","),
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], filename, { type: mime });
+}
+
+function uploadWebcamImage(image) {
+  console.log(image);
+  const url = "http://localhost:8000/webcam/upload/";
   const formData = new FormData();
   formData.append("file", image);
   formData.append("username", data.username());
@@ -17,7 +31,24 @@ function uploadImage(image) {
     .catch((err) => console.error(err));
 }
 
+function uploadImage(image) {
+  const url = "http://localhost:8000/image/uploaddesktop/";
+  const formData = new FormData();
+  formData.append("file", image);
+  formData.append("username", data.username());
+  console.log(image);
+  fetch(url, {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(`Successfully uploaded image: ${data}`))
+    .catch((err) => console.error(err));
+}
+
 const UploadPage = () => {
+  const webcamRef = useRef(null);
+  const [webcamImage, setWebcamImage] = useState("test");
   const navigate = useNavigate();
   const [fileURL, setFileURL] = useState();
   const [file, setFile] = useState();
@@ -25,9 +56,16 @@ const UploadPage = () => {
   const handleFileChange = (e) => {
     if (e.target.files) {
       setFile(e.target.files[0]);
+      console.log(e.target.files[0]);
       setFileURL(URL.createObjectURL(e.target.files[0]));
     }
   };
+
+  const capture = useCallback(() => {
+    const image = webcamRef.current.getScreenshot();
+    setWebcamImage(image);
+    uploadWebcamImage(dataURLtoFile(image, "webcam.png"));
+  }, [webcamRef, setWebcamImage]);
 
   return (
     <Center h="100vh">
@@ -45,9 +83,18 @@ const UploadPage = () => {
             <Box w="64" h="64" bg="gray"></Box>
           )}
         </Center>
+        <Center mt={5}>
+          <Webcam
+            width="50%"
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/png"
+          />
+        </Center>
         <Button
           mt={5}
           onClick={() => {
+            capture();
             uploadImage(file);
             navigate("/");
           }}
